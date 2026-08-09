bbf1f3ed6943dff72924d7ac472102607c525d7a
Samuel Dietz <samdietz2001@gmail.com>
2026-08-09 16:27:17 +1000

    fix-server-to-local-network

--- crates/protocol/src/lib.rs
+++ crates/protocol/src/lib.rs
@@ -1,4 +1,4 @@
-pub const SERVER_ADDRESS: &str = "127.0.0.1:3000";
+pub const SERVER_ADDRESS: &str = "0.0.0.0:3000";
 pub const WEBSOCKET_PATH: &str = "/ws";
 pub const SERVER_URL: &str = "ws://127.0.0.1:3000/ws";
 pub const LOGIN_PATH: &str = "/login";
